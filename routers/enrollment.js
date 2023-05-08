68753b35ecca5429fbd23252579c52a0095c8215
mdadul <emdadulislam580@gmail.com>
2023-05-08 06:51:19 +0600

    enrollment controller added

--- routers/enrollment.js
+++ routers/enrollment.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const enrollmentController = require("../controller/enrollment");
+const { ROLES } = require("../constants");
+const role = require("../middlewares/roles");
+const router = express.Router();
+
+router.get("/", auth,role.check(ROLES.ADMIN), enrollmentController.getAllEnrollments);
+router.get("/:id", auth, role.check(ROLES.ADMIN), enrollmentController.getEnrollmentById);
+router.post("/", auth, enrollmentController.createEnrollment);
+router.put("/:id", auth, role.check(ROLES.ADMIN), enrollmentController.updateEnrollment);
+router.delete("/:id", auth, role.check(ROLES.ADMIN), enrollmentController.deleteEnrollment);
